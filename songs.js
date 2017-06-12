34837bf35f4d774d08eacc97732fdccf0dc1727b
Joshua Lloyd <joshualloyd@joshualloyd.com>
2017-06-12 16:10:18 -0500

    added songs to beginning and end of array

--- songs.js
+++ songs.js
@@ -1,7 +1,9 @@
 var songs = [];
 
+songs[songs.length] = "Bohemian Rhapsody > by Queen";
 songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
 songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
 songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
 songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
 songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+songs[songs.length] = "I Would Do Anything For Love > by Meatloaf";
