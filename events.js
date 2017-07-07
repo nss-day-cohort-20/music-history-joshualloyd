a36d6ec8489419f6cef1acd53186c35a981af0c2
Joshua Lloyd <joshualloyd@joshualloyd.com>
2017-07-07 13:40:20 -0500

    can add songs to list

--- events.js
+++ events.js
@@ -2,6 +2,14 @@ var listLink = document.getElementById('list-link');
 var addLink = document.getElementById('add-link');
 var listView = document.getElementById('list-view');
 var addView = document.getElementById('add-view');
+var songNameInput = document.getElementById('song-name-input');
+var songArtistInput = document.getElementById('artist-input');
+var songAlbumInput = document.getElementById('album-input');
+var addBtn = document.getElementById('add-btn');
+
+window.addEventListener('load', function(){
+	printSongList(buildSongListString());
+});
 
 listLink.addEventListener('click', function(){
 	addView.classList.toggle('hidden');
@@ -11,4 +19,13 @@ listLink.addEventListener('click', function(){
 addLink.addEventListener('click', function(){
 	listView.classList.toggle('hidden');
 	addView.classList.toggle('hidden');
+});
+
+addBtn.addEventListener('click', function(){
+	var songString =`${songNameInput.value} > by ${songArtistInput.value} on the album ${songAlbumInput.value}`;//get form values
+	songs.push(songString);
+	songNameInput.value = '';
+	songArtistInput.value = '';
+	songAlbumInput.value = '';
+	printSongList(buildSongListString());
 });
--- songs.js
+++ songs.js
@@ -1,38 +1,40 @@
 var songs = [];
 
-songs[songs.length] = "Bohemian Rhapsody > by Queen on the album A Night at the Opera";
 songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
 songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
 songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
 songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
 songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
-songs[songs.length] = "I Would Do Anything For Love > by Meatloaf on the album Bat Out of Hell II: Back into Hell";
+
+songs.unshift("Bohemian Rhapsody > by Queen on the album A Night at the Opera");
+songs.push("I Would Do Anything For Love > by Meatloaf on the album Bat Out of Hell II: Back into Hell");
+
+function buildSongListString(){
+	let songHTMLString = '';
+
+	for ( var i = 0 ; i<songs.length ; i++ ) {
+
+		let fixedSongString = songs[i].replace('>','-').replace('*','').replace('@','').replace('!','').replace('(','');
+		let songName = fixedSongString.substring(0, fixedSongString.indexOf(' -'));
+		let artistName = fixedSongString.substring(fixedSongString.indexOf('by') + 2, fixedSongString.indexOf('on the album'));
+		let albumName = fixedSongString.substring(fixedSongString.indexOf('album') + 5, fixedSongString.length + 1);
+
+
+
+		songHTMLString +=
+		`
+		<li>
+			<h3>${songName}</h3>
+			<p>${artistName} |  ${albumName} | Genre</p>
+		</li>
+		`
+	}
+
+	return songHTMLString;
+}
 
 function printSongList(inputString) {
 	let songList = document.getElementById('song-list-ul');
 	songList.innerHTML = inputString;
 }
 
-
-
-let songHTMLString = '';
-
-for ( var i = 0 ; i<songs.length ; i++ ) {
-
-	let fixedSongString = songs[i].replace('>','-').replace('*','').replace('@','').replace('!','').replace('(','');
-	let songName = fixedSongString.substring(0, fixedSongString.indexOf(' -'));
-	let artistName = fixedSongString.substring(fixedSongString.indexOf('by') + 2, fixedSongString.indexOf('on the album'));
-	let albumName = fixedSongString.substring(fixedSongString.indexOf('album') + 5, fixedSongString.length + 1);
-
-
-
-	songHTMLString +=
-	`
-	<li>
-		<h3>${songName}</h3>
-		<p>${artistName} |  ${albumName} | Genre</p>
-	</li>
-	`
-}
-
-printSongList(songHTMLString);
